bc8f12d184e401f7384f4ff5ab17935875779f8e
Wenqiang Li <wenqiangli@WenqiangtekiMacBook-Pro.local>
2018-06-06 23:14:52 +0800

    feat(sort): add insertionSort
    增加了插入排序

--- sort/main.js
+++ sort/main.js
@@ -4,8 +4,10 @@ const sortMethods =  require('./sort_methods.js')
 function main() {
   let n = 10000
   let arr = sortTestHelper.generateRandomArray(n, 0, n)
+  let arr2 = JSON.parse(JSON.stringify(arr))
 
   sortTestHelper.testSort('选择排序', arr, n, sortMethods.selectionSort)
+  sortTestHelper.testSort('插入排序', arr2, n, sortMethods.insertionSort)
 
 }
 
--- sort/sort_methods.js
+++ sort/sort_methods.js
@@ -19,6 +19,22 @@ const sortMethods = {
     }
   },
 
+  /**
+   * 插入排序
+   * @param {Array} arr 
+   */
+  insertionSort(arr) {
+    let l = arr.length
+    let tempC
+    for(let i = 1; i < l; i++){
+      for(let j = i; j > 0 && arr[j] < arr [j - 1]; j--){
+        tempC = arr [j]
+        arr[j] = arr[j - 1]
+        arr[j - 1] = tempC
+      }
+    }
+  }
+
 }
 
 module.exports = sortMethods
