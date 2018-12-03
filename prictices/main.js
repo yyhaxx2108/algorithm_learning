8fb3b5e5aeb21d80891ed92e3c42effab887c218
yyhaxx2108 <yyhaxx@163.com>
2018-12-03 23:00:57 +0800

    feat(prictices): add binarySearch

--- prictices/main.js
+++ prictices/main.js
@@ -83,7 +83,28 @@ function findKthLargest(nums, k){
   }
 }
 
+// 在有序数组arr中寻找数t
+function binarySearch(arr, t){
+  // 初始化左值和右值
+  let l = 0, r = arr.length - 1
+  // 开始在 [l, r] 中查找
+  while(l <= r){
+    let mid = Math.floor((l + r) / 2)
+    if(arr[mid] === t){
+      return mid
+    }else if (arr[mid] > t){
+      r = mid - 1
+    }else {
+      l = mid + 1
+    }
+  }
+  return -1
+}
+
+
+let arr =  [1, 3, 5, 6, 7, 9, 12, 16]
+let t = 124
+console.log(binarySearch(arr, t))
 // let arr = [3,2,3,1,2,4,5,5,6]
 // let k = 4
-
 // console.log(findKthLargest(arr, k))
